3ac7a62aacbb0e878a3e93983f6ad85eff255ff1
roblesdotdev <httpsnip@gmail.com>
2022-10-17 09:05:07 -0300

    activities routes/controllers

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const getCountriesRoutes = require('./countries')
+const getActivitiesRoutes = require('./activities')
 
 function getApiRoutes() {
   const router = express.Router()
 
   router.use('/countries', getCountriesRoutes())
+  router.use('/activities', getActivitiesRoutes())
 
   router.use(function (_req, res) {
     res.jsonError(404, 'Sorry! Could not found page.')
