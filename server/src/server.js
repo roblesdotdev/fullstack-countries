e4e364b62ab5b6b0adf79034347bcc6ff2b3d02b
roblesdotdev <httpsnip@gmail.com>
2022-10-17 07:09:37 -0300

    server general config

--- server/src/server.js
+++ server/src/server.js
@@ -3,10 +3,27 @@ const logger = require('loglevel')
 const errorMiddleware = require('./middlewares/error-middleware')
 const errorBuilder = require('./middlewares/error-builder')
 const successBuilder = require('./middlewares/success-builder')
+const cors = require('cors')
 require('express-async-errors')
 
 function startServer({ port = process.env.PORT } = {}) {
   const app = express()
+
+  app.disable('x-powered-by')
+  app.use(express.urlencoded({ extended: true, limit: '50mb' }))
+  app.use(express.json({ limit: '50mb' }))
+  if (process.env.NODE_ENV === 'development') {
+    const morgan = require('morgan')
+    app.use(morgan('tiny'))
+  }
+  app.use(
+    cors({
+      origin: '*',
+      credentials: true,
+      allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
+      methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'DELETE'],
+    })
+  )
 
   app.use(errorBuilder)
   app.use(successBuilder)
