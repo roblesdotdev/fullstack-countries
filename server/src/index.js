2037a01b7ad3338e9bbe3a7b0ee106898027d963
roblesdotdev <httpsnip@gmail.com>
2022-10-17 06:23:08 -0300

    setup express server

---
+++ server/src/index.js
@@ -0,0 +1,6 @@
+const logger = require("loglevel");
+const { startServer } = require("./server");
+
+logger.setLevel("INFO");
+
+startServer({ port: 3001 });
---
+++ server/src/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const logger = require("loglevel");
+require("express-async-errors");
+
+function startServer({ port = process.env.PORT } = {}) {
+  const app = express();
+
+  app.get("/", (req, res) => res.json({ message: "Working" }));
+
+  return new Promise((resolve) => {
+    const server = app.listen(port, () => {
+      logger.info(`Server listening at ${server.address().port}`);
+    });
+
+    resolve(server);
+  });
+}
+
+module.exports = {
+  startServer,
+};
